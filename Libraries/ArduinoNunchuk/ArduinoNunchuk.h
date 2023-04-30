f140d03e441cb4218afc53be4485b574e9845e5e
Kevin Cogill <skwerl@gmail.com>
2023-04-30 18:09:30 -0500

    Updated libs

--- Libraries/ArduinoNunchuk/ArduinoNunchuk.h
+++ Libraries/ArduinoNunchuk/ArduinoNunchuk.h
@@ -1,29 +1,25 @@
 /*
  * ArduinoNunchuk.h - Improved Wii Nunchuk library for Arduino
- * 
- * Copyright 2011-2012 Gabriel Bianconi, http://www.gabrielbianconi.com/
+ *
+ * Copyright 2011-2013 Gabriel Bianconi, http://www.gabrielbianconi.com/
  *
  * Project URL: http://www.gabrielbianconi.com/projects/arduinonunchuk/
  *
- * Based on the following projects/websites:
+ * Based on the following resources:
  *   http://www.windmeadow.com/node/42
  *   http://todbot.com/blog/2008/02/18/wiichuck-wii-nunchuck-adapter-available/
  *   http://wiibrew.org/wiki/Wiimote/Extension_Controllers
- * 
+ *
  */
 
 #ifndef ArduinoNunchuk_H
 #define ArduinoNunchuk_H
 
-#if (ARDUINO >= 100)
-  #include <Arduino.h>
-#else
-  #include <WProgram.h>
-#endif
+#include <Arduino.h>
 
 class ArduinoNunchuk
-{  
-  public:  
+{
+  public:
     int analogX;
     int analogY;
     int accelX;
@@ -31,11 +27,11 @@ class ArduinoNunchuk
     int accelZ;
     int zButton;
     int cButton;
-  
-    void init();    
+
+    void init();
     void update();
-    
-  private:  
+
+  private:
     void _sendByte(byte data, byte location);
 };
 
